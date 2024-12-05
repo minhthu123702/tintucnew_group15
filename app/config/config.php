8e6a155d40876d825953b42f3c4dd9af83fbf517
minhthu123702 <nguyenminhthu191224@gmail.com>
2024-12-05 16:35:57 +0700

    thu

--- app/config/config.php
+++ app/config/config.php
@@ -2,8 +2,7 @@
 define('APP_ROOT', dirname(__FILE__, 3)); 
 define('DOMAIN', 'http://localhost/tintucnew_group15/');
 
-// Định nghĩa thông tin kết nối cơ sở dữ liệu
 define('DB_HOST', 'localhost'); 
 define('DB_USER', 'root');     
 define('DB_PASS', '');          
-define('DB_NAME', 'tlunews');    // Tên cơ sở dữ liệu
+define('DB_NAME', 'tlunews');   

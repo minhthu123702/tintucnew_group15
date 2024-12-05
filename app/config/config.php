dcdea948f33f1a0edd5310cd79f542ef99503b9e
minhthu123702 <nguyenminhthu191224@gmail.com>
2024-12-05 16:32:14 +0700

    update

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+define('APP_ROOT', dirname(__FILE__, 3)); 
+define('DOMAIN', 'http://localhost/tintucnew_group15/');
+
+// Định nghĩa thông tin kết nối cơ sở dữ liệu
+define('DB_HOST', 'localhost'); 
+define('DB_USER', 'root');     
+define('DB_PASS', '');          
+define('DB_NAME', 'tlunews');    // Tên cơ sở dữ liệu
--- app/config/database.php
+++ app/config/database.php
@@ -1,1 +1,38 @@
-git branch -d ten_branch
+<?php
+include_once 'config.php';
+
+class DBConnection {
+    private $host;
+    private $user;
+    private $pass;
+    private $dbname;
+    private $conn;
+    public function __construct() {
+        $this->host = DB_HOST;
+        $this->user = DB_USER;
+        $this->pass = DB_PASS;
+        $this->dbname = DB_NAME;
+
+        try {
+    
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->user, $this->pass);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            $this->conn = null;
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
+$db = new DBConnection();
+$conn = $db->getConnection();
+if ($conn) {
+    echo "Kết nối cơ sở dữ liệu thành công!";
+} else {
+    echo "Kết nối thất bại!";
+}
+?>
+

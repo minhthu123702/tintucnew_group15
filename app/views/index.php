817588017eba7eece661bc2daaf27e601e0c0046
Trung Kiin <136331695+Primeee9020398@users.noreply.github.com>
2024-12-05 16:19:36 +0700

    Update index.php

--- app/views/index.php
+++ app/views/index.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>TLU News - Trang tin tức</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    
+    <!-- Header -->
+    <?php include 'views/home/index.php'; ?>
 </body>
 </html>
